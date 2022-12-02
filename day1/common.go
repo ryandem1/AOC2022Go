20c5d66a87b39aacd6f7ffbd5f93637f48942305
ryandemarigny <ryandemarigny@gmail.com>
2022-12-01 21:38:36 -0800

    Implemented readElves()

---
+++ day1/common.go
@@ -0,0 +1,32 @@
+package day1
+
+import (
+	"github.com/ryandem1/aoc_2022_go/common"
+	"strconv"
+)
+
+// readElves will read lines from the input file, deserialize the data into an Elf and yield it
+func readElves() chan Elf {
+	elves := make(chan Elf)
+	go func() {
+		var foods []Food
+
+		for line := range common.ReadLinesFromFile("day1") {
+			if line == "" {
+				elf := Elf{Food: foods}
+				elves <- elf
+				foods = []Food{}
+			} else if calories, err := strconv.Atoi(line); err == nil {
+				foods = append(foods, Food{Calories: calories})
+			}
+		}
+
+		// This is to handle sending the very last item if there is no newline at the end of the file
+		if len(foods) > 0 {
+			elf := Elf{Food: foods}
+			elves <- elf
+		}
+		close(elves)
+	}()
+	return elves
+}
