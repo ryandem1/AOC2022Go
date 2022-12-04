bae182421bf6e9f42be960dd073c5777496ab2f0
ryandemarigny <ryandemarigny@gmail.com>
2022-12-04 08:43:35 -0800

    Implemented getPriorities

--- day3/models.go
+++ day3/models.go
@@ -16,3 +16,22 @@ func newRucksack(contents string) elfRucksack {
 	}
 	return rucksack
 }
+
+// getPriorities will return the map of priorities that each item has
+// Lowercase item types a through z have priorities 1 through 26.
+// Uppercase item types A through Z have priorities 27 through 52.
+func getPriorities() map[rune]int {
+	priorities := make(map[rune]int)
+	pValue := 0 // priority value
+
+	for letter := 'a'; letter <= 'z'; letter++ {
+		pValue++
+		priorities[letter] = pValue
+	}
+
+	for letter := 'A'; letter <= 'Z'; letter++ {
+		pValue++
+		priorities[letter] = pValue
+	}
+	return priorities
+}
