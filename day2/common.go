84217e071e69afcc724b3d160e0b1639de101a52
ryandemarigny <ryandemarigny@gmail.com>
2022-12-03 21:45:01 -0800

    Completed day 2 Part1

--- day2/common.go
+++ day2/common.go
@@ -6,24 +6,24 @@ import (
 	"strings"
 )
 
-func winningShape(shape1 handShape, shape2 handShape) *handShape {
-	if shape1 == shape2 {
+func winningShape(shape1 *handShape, shape2 *handShape) *handShape {
+	if *shape1 == *shape2 {
 		return nil
 	}
-	winner := &shape2
+	winner := shape2
 
-	switch shape1 {
+	switch *shape1 {
 	case rock:
-		if shape2 == scissors {
-			winner = &shape1
+		if *shape2 == scissors {
+			winner = shape1
 		}
 	case paper:
-		if shape2 == rock {
-			winner = &shape1
+		if *shape2 == rock {
+			winner = shape1
 		}
 	case scissors:
-		if shape2 == paper {
-			winner = &shape1
+		if *shape2 == paper {
+			winner = shape1
 		}
 	default:
 		log.Fatal("Unrecognized shape! How is this possible??")
@@ -46,4 +46,31 @@ func getRounds() chan RPSRound {
 		close(rounds)
 	}()
 	return rounds
+}
+
+// scoreRound will rake in a round and perform the scoring logic to output an int score. Scoring logic is as follows:
+//
+// The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
+// plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+func scoreRound(round RPSRound) (score int) {
+	switch round.strategyShape {
+	case rock:
+		score += 1
+	case paper:
+		score += 2
+	case scissors:
+		score += 3
+	}
+
+	winner := winningShape(&round.strategyShape, &round.opponentShape)
+	switch winner {
+	case nil: // tie
+		score += 3
+	case &round.strategyShape:
+		score += 6
+	case &round.opponentShape:
+		score += 0
+	}
+
+	return score
 }
