0d14df9d9e3ecead9c078bcf05a73f13d2ce91ac
ryandemarigny <ryandemarigny@gmail.com>
2022-12-03 20:51:20 -0800

    Implemented models for day2

--- day2/models.go
+++ day2/models.go
@@ -1,5 +1,9 @@
 package day2
 
+import (
+	"fmt"
+)
+
 type handShape int32
 
 const (
@@ -8,6 +12,21 @@ const (
 	scissors
 )
 
-type player struct {
-	score int
+// letterToHandShape will convert the letter from the input file to a handShape
+func letterToHandShape(letter string) handShape {
+	switch letter {
+	case "A", "X":
+		return rock
+	case "B", "Y":
+		return paper
+	case "C", "Z":
+		return scissors
+	default:
+		panic(fmt.Sprintf("Unhandled letter %s", letter))
+	}
+}
+
+type round struct {
+	opponentShape handShape
+	strategyShape handShape
 }
