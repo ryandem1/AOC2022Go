822b5e24c46e9ce9ccfd3587fd859a9d4b5d181c
ryandemarigny <ryandemarigny@gmail.com>
2022-12-04 08:55:23 -0800

    Bug fix

--- day2/puzzle.go
+++ day2/puzzle.go
@@ -1,8 +1,10 @@
 package day2
 
+import "github.com/ryandem1/aoc_2022_go/common"
+
 // Part1 What would your total score be if everything goes exactly according to your strategy guide?
 func Part1() (totalScore int) {
-	for round := range getRounds(part1) {
+	for round := range getRounds(common.Part1) {
 		totalScore += scoreRound(round)
 	}
 	return totalScore
@@ -10,7 +12,7 @@ func Part1() (totalScore int) {
 
 // Part2 What would your total score be if everything goes exactly according to your strategy guide?
 func Part2() (totalScore int) {
-	for round := range getRounds(part2) {
+	for round := range getRounds(common.Part2) {
 		totalScore += scoreRound(round)
 	}
 	return totalScore
