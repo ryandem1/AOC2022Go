4b83d35d827f0203e2fd158b51bd99b6283c2be0
ryandemarigny <ryandemarigny@gmail.com>
2022-12-04 08:55:54 -0800

    Implemented `readRucksacks()`

--- day3/common.go
+++ day3/common.go
@@ -1,1 +1,17 @@
 package day3
+
+import "github.com/ryandem1/aoc_2022_go/common"
+
+// readRucksacks will parse the input file for elfRucksack objects and send them through a channel
+func readRucksacks() chan elfRucksack {
+	rucksacks := make(chan elfRucksack)
+
+	go func() {
+		for line := range common.ReadLinesFromFile("day3") {
+			rucksack := newRucksack(line)
+			rucksacks <- rucksack
+		}
+		close(rucksacks)
+	}()
+	return rucksacks
+}
