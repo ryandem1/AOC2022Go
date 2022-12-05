17b5b6d74c90d494e03fd383a422632f28652438
ryandemarigny <ryandemarigny@gmail.com>
2022-12-04 18:33:33 -0800

    Completed Part1 of day4

--- day4/puzzle.go
+++ day4/puzzle.go
@@ -1,5 +1,13 @@
 package day4
 
 func Part1() (fullyOverlappingPairsCount int) {
+	for pair := range readCleaningAssignmentPairs() {
+		for i := 0; i < 2; i++ {
+			if pair[i].start <= pair[i^1].start && pair[i].end >= pair[i^1].end {
+				fullyOverlappingPairsCount++
+				break
+			}
+		}
+	}
 	return fullyOverlappingPairsCount
 }
