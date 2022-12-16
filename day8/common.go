e7b8a816df962120bacd7938a1778d596c5a620d
ryandemarigny <ryandemarigny@gmail.com>
2022-12-15 21:30:58 -0800

    Implemented isVisible

--- day8/common.go
+++ day8/common.go
@@ -1,8 +1,19 @@
 package day8
 
 import (
+	"fmt"
 	"github.com/ryandem1/aoc_2022_go/common"
 	"strconv"
+)
+
+type viewAngle int32
+
+// viewAngle is the different angles that you can try to view trees from
+const (
+	top viewAngle = iota
+	bottom
+	left
+	right
 )
 
 // buildTreePlane will read the input and build the 2D array of treeHeight values
@@ -21,4 +32,43 @@ func buildTreePlane() (plane [][]treeHeight) {
 		plane = append(plane, row)
 	}
 	return plane
+}
+
+// isVisible will check if a tree (by x, y) is visible in a plane from a certain angle
+func isVisible(x int, y int, height treeHeight, plane [][]treeHeight, angle viewAngle) (visible bool) {
+	visible = true
+
+	switch angle {
+	case top:
+		for iy := y - 1; iy > 0; iy-- {
+			if plane[x][iy] >= height {
+				visible = false
+				break
+			}
+		}
+	case bottom:
+		for iy := y + 1; iy > len(plane); iy++ {
+			if plane[x][iy] >= height {
+				visible = false
+				break
+			}
+		}
+	case left:
+		for ix := x - 1; ix > 0; ix-- {
+			if plane[ix][y] >= height {
+				visible = false
+				break
+			}
+		}
+	case right:
+		for ix := x + 1; ix > len(plane[0]); ix++ {
+			if plane[ix][y] >= height {
+				visible = false
+				break
+			}
+		}
+	default:
+		panic(fmt.Sprintf("Unrecognized angle: %d", angle))
+	}
+	return visible
 }
--- day8/puzzle.go
+++ day8/puzzle.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"fmt"
 	"github.com/ryandem1/aoc_2022_go/common"
 )
 
@@ -10,6 +11,11 @@ Consider your map; how many trees are visible from
 outside the grid?
 `
 
-	_ = buildTreeMap()
+	trees := buildTreePlane()
+	for y, treeRow := range trees {
+		for x, height := range treeRow {
+			fmt.Println(isVisible(x, y, height, trees, top), isVisible(x, y, height, trees, bottom), isVisible(x, y, height, trees, left), isVisible(x, y, height, trees, right))
+		}
+	}
 	return solution
 }
