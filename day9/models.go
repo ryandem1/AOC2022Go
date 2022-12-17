445df06be108141313c74505a61c0eb4825a1877
ryandemarigny <ryandemarigny@gmail.com>
2022-12-17 09:59:01 -0800

    More day9 models

--- day9/models.go
+++ day9/models.go
@@ -1,5 +1,14 @@
 package day9
 
+type motionDirection int32
+
+const (
+	up motionDirection = iota
+	down
+	left
+	right
+)
+
 type coords struct {
 	x int
 	y int
@@ -8,4 +17,9 @@ type coords struct {
 type bridgeRope struct {
 	headPos coords
 	tailPos coords
+}
+
+type ropeMotion struct {
+	direction motionDirection
+	amount    int
 }
